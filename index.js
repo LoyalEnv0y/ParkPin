b226c39a20b35181586c40fb997219b47d90761a
LoyalEnv0y <cetin200012@gmail.com>
2023-07-02 13:38:57 +0300

    Fixing helmet

--- index.js
+++ index.js
@@ -58,9 +58,53 @@ const sessionConfig = {
 const helmet = require('helmet');
 app.use(session(sessionConfig));
 app.use(flash());
+const scriptSrcUrls = [
+	"https://stackpath.bootstrapcdn.com/",
+	"https://api.tiles.mapbox.com/",
+	"https://api.mapbox.com/",
+	"https://kit.fontawesome.com/",
+	"https://cdnjs.cloudflare.com/",
+	"https://cdn.jsdelivr.net/",
+	"https://res.cloudinary.com/dv5vm4sqh/"
+];
+const styleSrcUrls = [
+	"https://kit-free.fontawesome.com/",
+	"https://stackpath.bootstrapcdn.com/",
+	"https://api.mapbox.com/",
+	"https://api.tiles.mapbox.com/",
+	"https://fonts.googleapis.com/",
+	"https://use.fontawesome.com/",
+	"https://cdn.jsdelivr.net/",
+	"https://res.cloudinary.com/dv5vm4sqh/"
+];
+const connectSrcUrls = [
+	"https://*.tiles.mapbox.com",
+	"https://api.mapbox.com",
+	"https://events.mapbox.com",
+	"https://res.cloudinary.com/dv5vm4sqh/"
+];
+const fontSrcUrls = ["https://res.cloudinary.com/dv5vm4sqh/"];
+
 app.use(
-	helmet({
-		contentSecurityPolicy: false,
+	helmet.contentSecurityPolicy({
+		directives: {
+			defaultSrc: [],
+			connectSrc: ["'self'", ...connectSrcUrls],
+			scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+			styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+			workerSrc: ["'self'", "blob:"],
+			objectSrc: [],
+			imgSrc: [
+				"'self'",
+				"blob:",
+				"data:",
+				"https://res.cloudinary.com/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+				"https://images.unsplash.com/"
+			],
+			fontSrc: ["'self'", ...fontSrcUrls],
+			mediaSrc: ["https://res.cloudinary.com/dv5vm4sqh/"],
+			childSrc: ["blob:"]
+		}
 	})
 );
 
